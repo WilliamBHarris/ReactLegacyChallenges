61b35542dd5879557ff5639c6e24e680e60ae1fd
Will Harris <bharris010486@gmail.com>
2022-01-10 21:28:15 -0500

    Finished Search Challenge

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -1,26 +1,81 @@
-import React, {Component} from 'react';
-import {Input} from 'reactstrap';
- 
+import React, { Component } from "react";
+import { Input } from "reactstrap";
+
 export default class SearchIndex extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-     things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
-   }
+      things: [
+        "pen",
+        "marker",
+        "eraser",
+        "notebook",
+        "pencil",
+        "scissors",
+        "highlighter",
+        "stapler",
+        "paper clip",
+        "binder",
+        "hole punch",
+        "laminator",
+        "laminating sheets",
+        "protective sheets",
+        "index cards",
+      ],
+      search: "",
+    };
+    this.searchThings = this.searchThings.bind(this);
   }
-   
 
 
-//  function searchFunction() {
-//  }
+  searchThings(event) {
+    event.preventDefault();
+    this.setState({
+        search: ''
+    });
+    console.log(this.state.search)
+  }
 
- render() {
-   return(
-     <div>
-       <Input placeholder='Search Here' />
-       <h3>Results:</h3>
-     </div>
-     )
- }
+  changeHandler(event) {
+    this.setState({ search: event.target.value });
+  }
 
- }
+
+  render() {
+
+    return (
+      <div>
+        <form onSubmit={this.searchThings}>
+          <Input
+            type="text"
+            value={this.state.search}
+            onChange={(e) => this.changeHandler(e)}
+          />
+          {/* <button type="submit">Search</button> */}
+        </form>
+        <h3>Results:</h3>
+        <SearchRes search={this.state.search} things={this.state.things} />
+      </div>
+    );
+  }
+}
+
+const SearchRes = (props) => {
+    return (
+      <div>
+     <ThingRes search={props.search} things={props.things} />
+      </div>
+    );
+  }
+
+
+const ThingRes = (props) => {
+  return (
+    <div>
+      {props.things.includes(props.search) ? <h4>{props.search}</h4> : <h4 >{props.things.map((thing) => {
+        return <li>{thing}</li>
+      })}</h4>}
+      
+    </div>
+  );
+};
